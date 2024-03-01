8bdb6c58d1fbe036084eb92e85cbcb3f16756671
ETEMESI <samweletemesi2@Gmail.com>
2024-03-01 12:02:47 +0300

    Completed the lab assignment.

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,29 @@
-#!/usr/bin/env python3
+# #!/usr/bin/env python3
 
 class Book:
-    pass
-    
+    def __init__(self, title, page_count):
+        self.title = title
+        self._page_count = None
+        self.page_count = page_count
+
+    @property
+    def page_count(self):
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, value):
+        if not isinstance(value, int):
+            print("page_count must be an integer")
+        else:
+            self._page_count = value
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
+
+if __name__ == "__main__":
+    my_book = Book("Python Programming", 300)
+    my_book.page_count = "three hundred"
+
+    my_book.turn_page()
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,31 @@
-#!/usr/bin/env python3
-
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self._size = None
+        self.size = size
+        self.condition = "New"
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            print("size must be an integer")
+        else:
+            self._size = value
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"
+
+
+if __name__ == "__main__":
+    my_shoe = Shoe("Nike", 9)
+
+    my_shoe.size = "nine"
+
+    my_shoe.cobble()
+
+    print("Shoe condition:", my_shoe.condition)
